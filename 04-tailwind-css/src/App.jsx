34860873ff40ddbf5b408857d9f7ea486d487a70
Rohan Kegade <rohankegade123@gmail.com>
2026-01-01 19:54:05 +0530

    styling components using tailwind css

---
+++ 04-tailwind-css/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-white bg-black text-center p-5 rounded">
+        This is styled with tailwind css
+      </h1>
+    </>
+  );
+}
+
+export default App;
